60feec9c9b37137a35d796d4b662ac4b94ec18ac
xeranimus <nobodycaresaboutspam@gmail.com>
2019-01-14 19:16:14 -0500

    finished the tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,28 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents1 = fs::read_to_string(config.filename1)?;
+    let contents1 = config.filename1;
     let contents2 = fs::read_to_string(config.filename2)?;
+    
+    let results = if config.case_sensitive {
+        search(&contents1, &contents2)
+    } else {
+        search_case_insensitive(&contents1, &contents2)
+    };
 
-    println!("File1 data:\n{}", contents1);
-    println!("File2 data:\n{}", contents2);
+    for line in results {
+        println!("{}", line);
+    }
+
     Ok(())
 }
 
 pub struct Config {
     pub filename1: String,
     pub filename2: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -22,7 +32,69 @@ impl Config {
         }
         let filename1 = args[1].clone();
         let filename2 = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     
-        Ok(Config {filename1, filename2})
+        Ok(Config {filename1, filename2, case_sensitive})
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insenstive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
+
+fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+fn search_case_insensitive<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,13 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config  = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
-    println!("File1: {}", config.filename1);
-    println!("File2: {}", config.filename2);
     
     if let Err(e) = mergebg::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
 
         process::exit(1);
     }
