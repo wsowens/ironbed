8e0c415e6a261b5f2a9d85addf1c9ea62fce208b
xeranimus <nobodycaresaboutspam@gmail.com>
2019-05-16 11:39:22 -0400

    pared everything back to basics

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,3 @@
-use std::env;
-use std::process;
-
-extern crate mergebg;
-use mergebg::Config;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config  = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
-        process::exit(1);
-    });
-
-    
-    if let Err(e) = mergebg::run(config) {
-        eprintln!("Application Error: {}", e);
-
-        process::exit(1);
-    }
+    println!("Hey it's me!");
 }
