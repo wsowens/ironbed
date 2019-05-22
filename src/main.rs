6ebda6bcd0321b1680f3b999a19b528207c29017
xeranimus <nobodycaresaboutspam@gmail.com>
2019-05-22 13:48:28 -0400

    added extract_coords function

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use std::io::{BufRead, BufReader};
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let bedf = BedGraphReader::new(&args[1]);
+    let bgs: Vec<BedGraphReader> = args[1..].iter().map(|filename| BedGraphReader::new(filename).unwrap()).collect();
+    union(bgs);
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 struct ChromPos {
     chrom: String,
     start: u32,
@@ -20,20 +21,23 @@ impl fmt::Display for ChromPos {
     }
 }
 
+#[derive(Debug)]
 struct BedGraphReader {
-    // add in trait based generics?
     filename: String,
     reader: BufReader<File>,
+    // should this be lazily evaluated?
+    coords: Option<ChromPos>,
     lineno: u32,
 }
 
 impl BedGraphReader {
     fn new(fname: &String) -> Result<BedGraphReader, String> {
+        //consider removing... this information is in the reader
         let filename = fname.clone();
         match File::open(&fname) {
             Err(x) => Err(x.to_string()),
             Ok(handle) =>
-                 Ok( BedGraphReader{filename, reader: BufReader::new(handle), lineno: 0}   )
+                 Ok( BedGraphReader{filename, reader: BufReader::new(handle), coords: None, lineno: 0}   )
           }
     }
 }
@@ -50,6 +54,7 @@ impl Iterator for BedGraphReader {
                     0 => None,
                     _ => {
                         self.lineno += 1;
+                        self.coords = Some(extract_coords(&temp).unwrap());
                         Some(temp)
                     }
                 }
@@ -58,11 +63,33 @@ impl Iterator for BedGraphReader {
     }
 }
 
+fn extract_coords(line: &String) -> Result<ChromPos, String> {
+    //TODO: avoid iterating over the entire line by using enumerate
+    let cols:Vec<&str> = line.split_whitespace().collect();
+    if cols.len() < 3 {
+        Err(format!("Invalid number of columns [{}] in line:\n{}", cols.len(), line))
+    } else {
+        //use lifetimes to make this work, instead of copying the string
+        let chrom = cols[0].to_string();
+        //TODO: provide a more useful error message for these
+        let start: u32 = cols[1].parse().unwrap();
+        let stop:  u32 = cols[2].parse().unwrap();
+        Ok(ChromPos{chrom, start, stop})
+    }
+}
+
 fn total_lines(mut bg: BedGraphReader) -> u32 {
     for _ in &mut bg {
-
+        //iterate over the entire BedGraph
     }
     bg.lineno
+}
+
+fn union(bedgraphs: Vec<BedGraphReader>) {
+    //TODO: consider refining this into an iterator
+    for mut bg in bedgraphs {
+        bg.next();
+    }
 }
 
 #[cfg(test)]
@@ -76,5 +103,13 @@ mod tests {
         let mut bedgraph = BedGraphReader::new(&filename).unwrap();
         //check the number of lines
         assert_eq!(total_lines(bedgraph), 9);
+    }
+
+    #[test]
+    fn chrom_pos() {
+        let mut bedgraph = BedGraphReader::new(&"test/unionbedg/1+2+3.bg".to_string()).unwrap();
+        assert_eq!(bedgraph.coords, None);
+        bedgraph.next();
+        assert_eq!(bedgraph.coords, Some(ChromPos{chrom: "chr1".to_string(), start: 900, stop: 1000}))
     }
 }
